1a3ac6640c1fbf9d532f699fd421dfb984e9bf84
John <john.wong.h@gmail.com>
2017-07-19 01:30:27 +0000

    added migration file that adds 'famous_person_id' as FK

---
+++ migrations/20170719004558_add_FamousPersonIdToMilestones.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+return Promise.all([
+  knex.schema.table('milestones', function (table) {
+    table.integer('famous_person_id');
+    table.foreign('famous_person_id').references('famous_people.id')
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function (table) {
+      table.dropForeign('famous_person_id', 'milestones_famous_person_id_foreign')
+      table.dropColumn('famous_person_id')
+    })
+  ])
+};
